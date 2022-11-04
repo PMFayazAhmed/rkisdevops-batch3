259a4b44df3144427dd7b1847d60d533b7ae5388
PMFayazAhmed <91198298+PMFayazAhmed@users.noreply.github.com>
2022-11-04 10:07:08 +0530

    Update f1.java

--- f1.java
+++ f1.java
@@ -3,6 +3,6 @@ class f1
 public static void main (String args[])
 {
 System.out.println("Welcome to DEVOPS ");
-System.out.println("Thank you for joining GCP course");
+System.out.println("Thank you for joining GCP Devops course");
 }
 }
