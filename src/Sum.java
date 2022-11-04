b501ed9f6a3b492199f4c693e2fb9e9cd1b833b0
Fayaz <fana.fayaz@gmail.com>
2022-11-04 09:50:27 +0530

    Modified Addition logic to Subtraction

--- src/Sum.java
+++ src/Sum.java
@@ -8,7 +8,7 @@ System.out.println("Enter num1 value");
 num1=s.nextInt();
 System.out.println("Enter num2 value");
 num2=s.nextInt();
-res=num1+num2;
-System.out.println("Sum is : "+res);
+res=num1-num2;
+System.out.println("Difference is : "+res);
 }
 }
