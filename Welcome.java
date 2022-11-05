66e3b5d3c07895c4834e9a8371aaddd158a55fd7
Fayaz <fana.fayaz@gmail.com>
2022-11-05 09:11:37 +0530

    First commit
    Second commit
    
    Third commit
    
    Fourth commit
    
    Fifth commit

---
+++ Welcome.java
@@ -0,0 +1,15 @@
+class Welcome
+{
+	public static void main(String args[])
+	{
+		System.out.println("Hello World - Welcome Note1");
+		 System.out.println("Hello World - Welcome Note2");
+		 System.out.println("Hello World - Welcome Note3");
+		  System.out.println("Hello World - Welcome Note4");
+		   System.out.println("Hello World - Welcome Note2");
+
+
+
+
+	}
+}
